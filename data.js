ac504cc78f8f39b3369ac0ec6c27141e91bf5d00
Shubham-pb08 <110660618+Shubham-pb08@users.noreply.github.com>
2024-09-13 09:56:01 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,50 @@
+const itemList = {
+        "Drinks": [
+            "Pepsi Bottles", "Diet pepsi Bottles", "Pepsi zero bottles", "7UP bottles", 
+            "Ginger ale bottles", "Root beer bottles", "Aquafina bottles", "Clamato juice", 
+            "Apple juice", "Dole apple juice", "Dole orange juice"
+        ],
+        "Sauces": [
+            "Smokey BBQ sauce", "Honey garlic sauce", "Sweet & spicy thai sauce", "Teriyaki sauce",
+            "Hot sauce", "Mild sauce", "Medium sauce", "Italian sauce", "Apple sauce", "Plum sauce",
+            "Cranberry sauce", "Worcestershire sauce", "Ketchup bottles", "Ketchup portions", 
+            "Caramel topping", "Chocolate topping", "Plum sauce portions", "BBQ sauce portions", 
+            "Coleslaw dressing", "Mayonnaise dine in", "Chalet dressing dine in", "Mighty caesar dressing portions",
+            "Chalet dressing portions", "Italian dressing portions", "Balsamic dressing portions", 
+            "Sour cream portions", "Butter pots", "Sour cream 4L", "Garlic butter spread", "Margarine bucket"
+        ],
+        "Boxes": [
+            "Ala carte boxes", "Entree boxes", "Ala carte liners", "Hot side boxes", "Side salad boxes (24oz)", 
+            "Big salad boxes", "32oz bowl", "12oz bowl", "8oz cups", "3.5oz cups", "2oz cups", 
+            "1oz cups", "32oz lids", "12oz/8oz lids", "3.5oz/2oz lids", "1oz lids", "Paper bags large", 
+            "Paper bags medium", "Paper bags small", "Bowl holders", "3 in 1 cutlery set", "Soup spoons", 
+            "Kids cups", "Kids cups lids", "Straws"
+        ],
+        "Perishables": [
+            "Lettuce romaine", "Lettuce chopped", "Lettuce leaf", "Heritage blend", "Coleslaw cabbage", 
+            "Shredded carrots", "Tomatoes", "Red onions", "Spring onions", "Broccoli", "Cauliflower", 
+            "Lemons", "Cucumbers", "Jumbo fries potatoes", "Baked potatoes"
+        ],
+        "Frozen": [
+            "Ribs", "Wings", "Tenders", "Perogies", "Crispy sandwich", "Green beans", 
+            "Sweet potato fries", "Mashed potatoes", "Pot pie mix", "Mini donuts", "Apple pie", 
+            "Coconut pie", "Lemon pie", "Cheese cake", "Molten lava cake", "Broccoli soup mix", 
+            "Spring rolls sheets", "Tortilla 12”", "Ace buns 4”", "Kids buns 3.5”", "Dinner rolls", 
+            "Hoagie rolls", "White bread", "Beef burgers", "Mini burgers", "Veggie burgers", 
+            "Kids pizza", "Kids mac and cheese balls", "Whipped cream", "Pot pie lids sheet 36 Qt."
+        ],
+        "Seasonings": [
+            "Chalet seasoning", "Steak spice seasoning", "Garlic Mesquite seasoning", "Cajun seasoning", 
+            "Cinnamon sugar", "Sprinkles", "Oreo crunch", "Salt portions", "Pepper portions", 
+            "Greek seasoning", "Pepper jar"
+        ],
+        "Cook": [
+            "Chicken totes", "Chalet seasoning mix", "Gravy mix", "Chicken soup mix", "Rice pilaf seasoning", 
+            "Rice", "Salt", "Noodles"
+        ],
+        "Other": [
+            "Tea bags", "Coffee", "Decaf coffee", "Cotton candy", "Milkkettes", "Creamers", 
+            "Milk", "Chocolate milk", "Sugar", "Sweetener", "Crackers", "Croutons", "M & Ms", 
+            "Kids toys", "Napkins", "Swiss chalet napkins"
+        ]
+    };
